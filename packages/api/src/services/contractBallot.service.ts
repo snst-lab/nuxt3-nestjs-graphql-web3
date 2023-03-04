2301602fb340dcb5172c4a8d8fc4d82d8c1fc8f5
tanusuke <snst.lab@gmail.com>
2023-03-04 11:55:52 +0900

    :sparkles: add smartcontract service

---
+++ packages/api/src/services/contractBallot.service.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@nestjs/common';
+import { parseUnits, isAddress } from 'ethers/lib/utils';
+import { constants } from '@constants';
+import { runtimeTools } from '@tools/runtime';
+
+const { gasLimit } = constants.web3.number;
+const { toNumber, useContract, useToken } = runtimeTools.web3;
+
+const ballot = useContract('Ballot');
+const ballotToken = useToken('Token');
+
+@Injectable()
+export class ContractBallotService {
+  async vote(projectId: number, _amount: number) {
+    const amount = parseUnits(_amount.toString(), ballotToken().decimals);
+    await ballotToken().abi.approve(ballot().address, amount);
+    await ballot().vote(projectId, amount, { gasLimit });
+  }
+
+  async unvote(projectId: number, _amount: number) {
+    await ballot().unvote(projectId, _amount, { gasLimit });
+  }
+  /**
+   * １日１回実行
+   * 審査機関が投票を取り下げた場合に、取り下げた数量に応じて事務局から投票権トークンを再配布する
+   */
+  async reconcile() {
+    const pendingAirdropList = await ballot().getPendingAirdropList();
+    const supporterList = pendingAirdropList[0];
+
+    if (supporterList.length > 0) {
+      for (let i = supporterList.length - 1; i >= 0; i--) {
+        const supporter = pendingAirdropList[0][i];
+        const amount = pendingAirdropList[1][i];
+
+        if (isAddress(supporter) && toNumber(amount) > 0) {
+          await ballotToken('admin').abi.approve(ballot().address, amount);
+          await ballot('admin').reconcileAirdrop(supporter, amount, {
+            gasLimit,
+          });
+        }
+      }
+    }
+  }
+}
